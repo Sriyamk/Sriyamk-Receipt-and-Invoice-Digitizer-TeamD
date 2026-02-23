0abbc46d5fe73e6e011e1062d4c325aa2adf6a89
Sriyamk <sriya_mk@yahoo.com>
2026-02-23 11:00:26 +0530

    Updated project files and added Day11, Day12, Day13 notes

--- assignments/assignment_3/enu1.py
+++ assignments/assignment_3/enu1.py
@@ -1,4 +1,4 @@
-a = ["Sriya", "for", "Sriya"]
+a = ["S", "for", "Sriya"]
 
 # Iterating list using enumerate to get both index and element
 for i, name in enumerate(a):
@@ -6,3 +6,40 @@ for i, name in enumerate(a):
 
 # Converting to a list of tuples
 print(list(enumerate(a)))
+
+fruits = ["apple", "banana", "cherry"]
+
+for index, value in enumerate(fruits):
+    print(index, value)
+
+
+#isinstance
+x = 10
+print(isinstance(x, str)) #10 is not a string, returns False
+y= "Hello"
+print(isinstance(y,str)) #y is a strigng
+
+#issubclass
+class animal:
+    pass
+class dog(animal):
+    pass
+print(issubclass(dog, animal))
+
+#testing enumerate
+K = "bobby", "sally", "josh"
+for i, name in enumerate(K):
+    print(i,name)
+    
+
+# delattr
+class student:
+    name = "Sriya"
+    age = 20
+delattr(student,"name")
+
+#purposefully trying to access the deleted attribute to show that it has been removed
+try:
+    print(student.name)
+except AttributeError:
+    print("The attribute 'name' has been deleted.")
